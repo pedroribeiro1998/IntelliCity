b6ce1e99ceae2e4c1fbbde82abf627b950f7b5a6
Pedro Ribeiro <pmiguelribeiro@ipvc.pt>
2020-03-21 16:02:15 +0000

    Página de login

---
+++ app/src/main/java/com/pedroribeiro/intellicity/NotasActivity.java
@@ -0,0 +1,14 @@
+package com.pedroribeiro.intellicity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
